40a0af88abfa5113389c3e14ea3ce0022604a449
Anrich <xiliniucheng@gmail.com>
2014-05-06 22:07:06 +0800

    Revise the remarks.

--- Ansi.c
+++ Ansi.c
@@ -3,8 +3,9 @@
 #include <string.h>
 
 /*
+ * 这里示范的是宏定义时常用的一种技巧
  * 1) 下面的宏是一个非复合语句
- × 2) 为满足通用性和强壮性要求，不能对库的使用做任何假设
+ * 2) 为满足通用性和强壮性要求，不能对宏的使用方法做任何假设
  */
 #define PRINT_GREETINGS do { \
     printf("Hi there!\n"); \
@@ -12,7 +13,7 @@
 
 int main(const int argc, const char* const* argv)
 {
-    // macro
+    // Invoke function via macro
     PRINT_GREETINGS;
 
     return 0;
