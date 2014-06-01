eea3eaeabae499b08f609a685fa5c9d0fb4b21a1
Anrich <anrich@debian>
2014-06-01 21:56:16 +0800

    Star Wars the movie shows in the terminal, via bash.

--- Bash.sh
+++ Bash.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/sh
 
 echo "Hi there!"
+
+echo "\n\nNow Star Wars the movie starts!\n\n"
+
+sleep 5s
+
+echo "... via telnet."
+
+sleep 2s
+
+# Star Wars the movie, telnet version
+telnet towel.blinkenlights.nl
+
